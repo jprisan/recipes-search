753bf3f5ca940f52dbca59e509e687b58579b7d4
Jose Gaspar Prieto Sanroman <jprisan@MacBook-Pro-de-Jose.local>
2019-04-20 20:41:14 +0200

    finish changes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,20 +8,14 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  // working in local mode
-   recipes: Recipe[] = [];
 
-  // working from api
-  // recipes: any[] = [];
+  recipes: any[] = [];
 
   constructor(private recipeService: RecipeService,
               private router: Router) { }
 
   ngOnInit() {
-    // working in local mode
-    //  this.recipes = this.recipeService.getRecipes();
 
-    // working from api
     this.recipeService.getRecipes()
       .subscribe((data: any) => {
         console.log(data.results);
